5b764a03e45ba29b8b9cbc8ec29e8ece8a70e456
kurtCoding <kjulien7790@gmail.com>
2024-06-15 14:54:41 -0400

    my initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { nanoid } = require("nanoid");
+const products = require("./data/products.json");
+
+const inform = console.log;
+
+function run() {
+
+    
+    switch(userInput) {
+        case "index":
+        break;
+        case "show":
+        break;
+        case "create":
+        break;
+        case "update":
+        break;
+        case "destroy":
+        break;
+    }
+}
